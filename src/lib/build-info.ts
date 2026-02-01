e76bfa9b04959fdd20af8630d20d50cb72c6704d
Bob Matsuoka <bob@matsuoka.com>
2026-02-01 14:52:17 -0500

    chore: bump version to 1.0.18

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -4,10 +4,10 @@
  */
 
 export const BUILD_INFO = {
-  version: '1.0.17',
-  gitHash: '0604171',
+  version: '1.0.18',
+  gitHash: '7e55362',
   gitBranch: 'main',
-  buildTime: '2026-02-01T19:49:51Z',
+  buildTime: '2026-02-01T19:52:35Z',
   nodeVersion: 'v25.2.1',
   isDirty: false,
 } as const;
